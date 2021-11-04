784b3104141c59f6abe0fdbb7f9cca3642d6c850
alekk12 <a7kolakowska@gmail.com>
2021-11-04 20:21:36 +0100

    reading and choosing the version done, saving kinda done

--- algorithm.py
+++ algorithm.py
@@ -1,24 +1,38 @@
-APPROXIMATE = 1 #Maybe should be passed as an command line argument
+from typing import List
+import hashlib
+import pathlib
+import sys
+from pathlib import Path
+
+USAGE = f"Usage: python {sys.argv[0]} [filename | filename -a | filename -e]"
+#no option results in exact solution
 
 # Helper functions
+
+
 def GenerateBaseSequence(size):
     return [i for i in range(size)]
 
-def CreateSequences(sequencesList, baseSequence, firstElement = 0):
+
+def CreateSequences(sequencesList, baseSequence, firstElement=0):
     if firstElement == len(baseSequence):
         sequencesList.append(baseSequence)
     else:
         for i in range(firstElement, len(baseSequence)):
             baseSequence[i], baseSequence[firstElement] = baseSequence[firstElement], baseSequence[i]
-            CreateSequences(sequencesList, baseSequence.copy(), firstElement + 1)
+            CreateSequences(sequencesList, baseSequence.copy(),
+                            firstElement + 1)
             baseSequence[i], baseSequence[firstElement] = baseSequence[firstElement], baseSequence[i]
 
+
 def GenerateMatrixFromSequenceCombination(matrix, sequence):
-    returnedMatrix = [[0 for _ in range(len(sequence))] for _ in range(len(sequence))]
+    returnedMatrix = [[0 for _ in range(len(sequence))]
+                      for _ in range(len(sequence))]
     for i in range(len(sequence)):
         for j in range(len(sequence)):
             returnedMatrix[i][j] = matrix[sequence[i]][sequence[j]]
     return returnedMatrix
+
 
 def CalculateCommonEdges(matrix, smallerMatrixSize):
     commonEdges = 0
@@ -28,6 +42,7 @@ def CalculateCommonEdges(matrix, smallerMatrixSize):
                 commonEdges += 1
     return commonEdges
 
+
 def CalculateRowDegree(matrix, rowNumber):
     row = matrix[rowNumber]
     sum = 0
@@ -35,6 +50,7 @@ def CalculateRowDegree(matrix, rowNumber):
         if i > 0:
             sum += 1
     return sum
+
 
 def FindMaxNotUsedDegree(matrix, matrixSize, sequence):
     maxDeg = -1
@@ -48,75 +64,107 @@ def FindMaxNotUsedDegree(matrix, matrixSize, sequence):
             index = i
     return index
 
+
 def GenerateSequenceOfLabelsFromMaxDegree(matrix, matrixSize):
     sequence = []
     while len(sequence) < matrixSize:
         sequence.append(FindMaxNotUsedDegree(matrix, matrixSize, sequence))
     return sequence
 
+
 def GenerateApproximateSequence(matrixBigger, matrixBiggerSize, matrixSmaller, matrixSmallerSize):
-    sequenceBigger = GenerateSequenceOfLabelsFromMaxDegree(matrixBigger, matrixBiggerSize)
-    sequenceSmaller = GenerateSequenceOfLabelsFromMaxDegree(matrixSmaller, matrixSmallerSize)
+    sequenceBigger = GenerateSequenceOfLabelsFromMaxDegree(
+        matrixBigger, matrixBiggerSize)
+    sequenceSmaller = GenerateSequenceOfLabelsFromMaxDegree(
+        matrixSmaller, matrixSmallerSize)
     realSequence = [-1 for i in range(matrixBiggerSize)]
     for i, j in zip(sequenceSmaller, range(len(sequenceSmaller))):
         realSequence[i] = sequenceBigger[j]
     for i in range(len(sequenceSmaller), len(sequenceBigger)):
         realSequence[i] = sequenceBigger[i]
     return realSequence
- 
- 
+
+
 # Read and Print
-def ReadMatrices():
-    biggerMatrix = [[0,1,0,0,1],
-                    [1,0,1,0,1],
-                    [0,1,0,1,0],
-                    [0,0,1,0,1],
-                    [1,1,0,1,0]]
-    biggerMatrixSize = 5
-    smallerMatrix = [[0,1,0,1],
-                     [1,0,1,1],
-                     [0,1,0,1],
-                     [1,1,1,0]]
-    smallerMatrixSize = 4
-    return (biggerMatrix, biggerMatrixSize, smallerMatrix, smallerMatrixSize)
+def ReadMatrices(path):
+    file = open(path, "r")
+    firstMatrixSize = int(file.readline().split(" ")[0])
+    firstMatrix = []
+    count = 0
+    while count < firstMatrixSize:
+        line = file.readline()
+        firstMatrix.append([int(j) for j in line.split()])
+        if not line:
+            break
+        count+=1
+    secondMatrixSize = int(file.readline().split(" ")[0])
+    secondMatrix = []
+    while True:
+        line = file.readline()
+        secondMatrix.append([int(j) for j in line.split()])
+        if not line:
+            break
+    file.close()
 
-def SaveAnswer(commonEdges, mappingSequence, graph):
-    if APPROXIMATE:
-        print("APPROXMIATE")
+    if firstMatrixSize > secondMatrixSize:
+        return (firstMatrix, firstMatrixSize, secondMatrix, secondMatrixSize)
     else:
-        print("EXACT")
-    print(commonEdges)
-    print(mappingSequence)
-    print(graph)
+        return (secondMatrix, secondMatrixSize, firstMatrix, firstMatrixSize)
 
+
+
+def SaveAnswer(commonEdges, mappingSequence, graph, approximate):
+    with open('answer.txt', 'w') as f:
+        if approximate:
+            f.write("APPROXIMATE"+ '\n')
+        else:
+            f.write("EXACT"+ '\n')
+        f.write(str(commonEdges)+ '\n')
+        f.write(" ".join(str(j) for j in mappingSequence)+ '\n')
+        for row in graph:
+            f.write(" ".join(str(item) for item in row) + '\n')
 
 # Algorithm
-def main():
-    (biggerMatrix, biggerMatrixSize, smallerMatrix, smallerMatrixSize) = ReadMatrices()
-
-    sequenceList = []
-
-    if APPROXIMATE:
-        sequenceList = [GenerateApproximateSequence(biggerMatrix, biggerMatrixSize, smallerMatrix, smallerMatrixSize)]
+def main(input_list: List[str]) -> None:
+    if len(input_list) < 1:
+        print(USAGE)
     else:
-        CreateSequences(sequenceList, GenerateBaseSequence(biggerMatrixSize))
+        opts = [opt for opt in input_list if opt.startswith("-")]
+        input_file = Path(input_list[0])
+        if not input_file.is_file():
+            print("Wrong file!")        
+        approximate = 0
+        if "-a" in opts:
+            approximate = 1
 
-    mostMatchingSequence = []
-    smallestCommonSupergaph = []
-    mostMatchingEdges = 0
+        (biggerMatrix, biggerMatrixSize, smallerMatrix,smallerMatrixSize) = ReadMatrices(input_file)
 
-    for sequence in sequenceList:
-        rearrangedMatrix = GenerateMatrixFromSequenceCombination(biggerMatrix, sequence)
-        for i in range(smallerMatrixSize):
-            for j in range(smallerMatrixSize):
-                rearrangedMatrix[i][j] += smallerMatrix[i][j]
-        commonEdges = CalculateCommonEdges(rearrangedMatrix, smallerMatrixSize)
-        if commonEdges > mostMatchingEdges:
-            mostMatchingEdges = commonEdges
-            mostMatchingSequence = sequence
-            smallestCommonSupergaph = rearrangedMatrix
-            
-    SaveAnswer(mostMatchingEdges, mostMatchingSequence, smallestCommonSupergaph)
+        sequenceList = []
+
+        if approximate:
+            sequenceList = [GenerateApproximateSequence(
+                biggerMatrix, biggerMatrixSize, smallerMatrix, smallerMatrixSize)]
+        else:
+            CreateSequences(sequenceList, GenerateBaseSequence(biggerMatrixSize))
+
+        mostMatchingSequence = []
+        smallestCommonSupergaph = []
+        mostMatchingEdges = 0
+
+        for sequence in sequenceList:
+            rearrangedMatrix = GenerateMatrixFromSequenceCombination(
+                biggerMatrix, sequence)
+            for i in range(smallerMatrixSize):
+                for j in range(smallerMatrixSize):
+                    rearrangedMatrix[i][j] += smallerMatrix[i][j]
+            commonEdges = CalculateCommonEdges(rearrangedMatrix, smallerMatrixSize)
+            if commonEdges > mostMatchingEdges:
+                mostMatchingEdges = commonEdges
+                mostMatchingSequence = sequence
+                smallestCommonSupergaph = rearrangedMatrix
+
+        SaveAnswer(mostMatchingEdges, mostMatchingSequence,
+                smallestCommonSupergaph, approximate)
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1:])
